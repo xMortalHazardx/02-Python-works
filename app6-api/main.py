ab0ada3e9329a385c322dd18318e1c57de8e292e
xmortalhazardx <cask.overpower_13@hotmail.com>
2024-07-22 17:21:09 -0300

    Criando API com Flask Step 1

--- app6-api/main.py
+++ app6-api/main.py
@@ -1,17 +1,17 @@
+import pandas
 from flask import Flask, render_template
 
-app = Flask('Website')
+app = Flask("Website")
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact_us")
-def contact():
-    return render_template("contact_us.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 app.run(debug=True)
